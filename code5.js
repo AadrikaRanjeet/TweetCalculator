ea4eae5d88cb9817eee1bc3a60033eb6cef1ae9f
AadrikaRanjeet <119300980+AadrikaRanjeet@users.noreply.github.com>
2023-01-25 19:35:46 +0530

    Add files via upload

---
+++ code5.js
@@ -0,0 +1,5 @@
+
+var tweet=prompt("enter a tweet");
+var tweetsize=tweet.length;
+alert("you have a word limit of 280");
+alert("You have written"+""+tweetsize+"words and now out of 140 you are left with "+(280-tweetsize));
